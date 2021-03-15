fe28db82b3646d5e7068f17ed046742b81336187
James Swank <jmswank7@gmail.com>
2021-03-15 17:29:17 -0600

    graphs pop data

--- application.py
+++ application.py
@@ -2,8 +2,30 @@ import dash
 import pandas as pd
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly.express as px
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
 
 
-new_df = pd.read_csv('pop.csv')
-print(new_df.tail())
+df = pd.read_csv('pop.csv')
+print(df.tail())
+
+fig = px.bar(df, x='year', y='totalPopulation')
+
+app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+  dcc.Graph(
+    id='pop-graph',
+    figure=fig
+  )
+])
+
+if __name__ == '__main__':
+    app.run_server(port=8080, debug=True)
 
