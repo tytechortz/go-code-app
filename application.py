1fbd0b1a6c8f1eb2109e79d8469fab9b8f9d37a2
James Swank <jmswank7@gmail.com>
2021-03-15 20:18:43 -0600

    county pop graph works with dropdown

--- application.py
+++ application.py
@@ -2,6 +2,7 @@ import dash
 import pandas as pd
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output, State
 import plotly.express as px
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -20,29 +21,25 @@ counties = []
 for i in df.county.unique():
   counties.append(i)
 
-print(counties)
-selected_cnty = 'Denver'
-
-
-print(df.tail())
-df = df[df['county']==selected_cnty]
-
-fig = px.bar(df, x='year', y='totalPopulation')
-
-
-
-
-
 app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
-  dcc.Graph(
-    id='pop-graph',
-    figure=fig
-  ),
-  dcc.Dropdown(
-    id='county',
-    options=[{'label':i, 'value':i} for i in counties],
-  ),
+     dcc.Graph(
+          id='county-pop-graph',
+     ),
+     dcc.Dropdown(
+          id='county',
+          options=[{'label':i, 'value':i} for i in counties],
+     ),
 ])
+
+@app.callback(
+     Output('county-pop-graph', 'figure'),
+     Input('county', 'value'))
+def display_cnty_pop(selected_county):
+     df_county_pop = df[df['county'] == selected_county]
+
+     fig = px.bar(df_county_pop, x='year', y='totalPopulation')
+
+     return fig
 
 if __name__ == '__main__':
     app.run_server(port=8080, debug=True)
